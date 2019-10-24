746fb2e478b62ed99393b51d2b65077e28866a69
Ygor Lemos <ygbr@mac.com>
2019-10-24 14:44:27 -0300

    Update 00_empty_test.py

--- tests/00_empty_test.py
+++ tests/00_empty_test.py
@@ -1,1 +1,10 @@
+# tests/00_empty_test.py
+def test_empty():
+    """
+    PyTest tests are callables whose names start with "test"
+    (by default)
 
+    It looks for them in modules whose name contains "test"
+    (by default)
+    """
+    pass
